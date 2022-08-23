662f44b37a72283b7d69496986879d1efb840263
Yura <kasitsyn@gmail.com>
2022-08-23 08:32:23 +0700

    feat: change server link

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '762e469dced142a7a6a4fa3a44cb9635', // получите свой ключ https://newsapi.org/
         });
     }
